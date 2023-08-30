cf8e8b02d078ae907b58dbbb0c20c12051274dbe
priyanshu1602 <priyanshu0549@gmail.com>
2023-08-30 13:07:54 +0530

    added snomed folder

---
+++ BahmniSnomedUI/data.js
@@ -0,0 +1,2 @@
+var tags = ["snomed", "ui"]
+var specs = [{"path":"specs/UI/CdssAlert.html","name":"Snomed CDSS Diagnosis-Drug Alert","scenarios":[{"name":"Doctor should be able to get contraindication critical alert for the diagnosis-drugs interaction","tags":["snomed","ui"]},{"name":"Doctor should be able to get contraindication warning alert for the diagnosis-drugs interaction","tags":["snomed","ui"]}]}]
